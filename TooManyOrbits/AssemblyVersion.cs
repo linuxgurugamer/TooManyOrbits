9f738d84f672da474a00f1963ec5d98e37af72ef
linuxgurugamer@gmail.com <linuxgurugamer@gmail.com>
2025-06-15 06:55:28 -0400

    fixed reference for build

--- TooManyOrbits/AssemblyVersion.cs
+++ TooManyOrbits/AssemblyVersion.cs
@@ -5,5 +5,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyFileVersion("1.1.6.6")]
- [assembly: AssemblyVersion("1.1.6.6")]
+ [assembly: AssemblyFileVersion("1.1.7.0")]
+ [assembly: AssemblyVersion("1.1.7.0")]
